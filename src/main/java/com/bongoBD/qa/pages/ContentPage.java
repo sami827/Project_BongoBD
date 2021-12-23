661e84d9ea7a38275a091e4f6c7ceec56245db9c
Saubiya Jahan <saubiyajahan@gmail.com>
2021-12-23 23:12:00 +0600

    Add Action class to forward video

--- src/main/java/com/bongoBD/qa/pages/ContentPage.java
+++ src/main/java/com/bongoBD/qa/pages/ContentPage.java
@@ -4,7 +4,8 @@ import com.bongoBD.qa.base.BaseClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class ContentPage extends BaseClass {
@@ -14,6 +15,7 @@ public class ContentPage extends BaseClass {
     private String SKIP_AD = "//div[contains(text(),'Skip Ad')]"; //xpath of Skip ad button
     private String FORWARD_BUTTON = "//button[contains(@class,'forward_button')]"; //xpath of forward button
     private String VOLUME_BUTTON = "//button[@title='Mute']"; //xpath of volume button
+    private String PROGRESS_BAR = "//div[contains(@class,'vjs-play-progress')]"; //xpath
 
 
     //Returning boolean if the content title is displayed
@@ -25,6 +27,10 @@ public class ContentPage extends BaseClass {
     }
 
     public void interactingVideo() throws InterruptedException {
+        WebElement slider = driver.findElement(By.xpath(PROGRESS_BAR));
+        Actions action = new Actions(driver);
+        action.dragAndDropBy(slider, 400, 0).build().perform();
+
         WebElement volumeBtn = driver.findElement(By.xpath(VOLUME_BUTTON));
         volumeBtn.sendKeys(Keys.ARROW_DOWN);
         Thread.sleep(2000);
